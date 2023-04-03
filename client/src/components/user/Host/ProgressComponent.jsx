e63eba13cbc1d0e9c3e9976b565eefb35bb73f20
Mohammed irfad <muhammedirfad43@gmail.com>
2023-04-03 10:56:19 +0530

    host setup

--- client/src/components/user/Host/ProgressComponent.jsx
+++ client/src/components/user/Host/ProgressComponent.jsx
@@ -12,6 +12,8 @@ function ProgressComponent({link,step}) {
   const [currentStep, setCurrentStep] = useState(0);
   const [progress, setProgress] = useState(0);
 
+  
+
   function handleNext() {
 
     setCurrentStep(currentStep + 1);
@@ -24,6 +26,7 @@ function ProgressComponent({link,step}) {
     setProgress(progress - (100 / numSteps));
   }
   const numSteps = 10; 
+
   return (
    
     
@@ -41,9 +44,9 @@ function ProgressComponent({link,step}) {
 
      <div className='absolute right-0 bg-black flex mx-10 justtify-center mt-5 w-24 h-10 rounded-md'>
      {currentStep < numSteps && (
-        <a href={link}>
+     
         <button className='text-white font-lg justify-center item-center mx-8 h-10 mt-0' onClick={handleNext}>Next</button>
-        </a>
+    
         )}
      </div>
 
--- client/src/components/user/Host/Publish.jsx
+++ client/src/components/user/Host/Publish.jsx
@@ -17,6 +17,19 @@ function Publish(){
         autoplay
       />
             </div>
+                <div className='bg-black flex mb-5   selection: w-full md:w-1/2 justify-center '>
+                    <div className=' items-center flex flex-row bg-white justify-center mx-40 h-auto'>
+                   <div className='justify-start flex flex-col '>
+                   <h1 className='text-black font-semibold text-5xl text-start'>Congratulations,</h1>
+                   <h1 className='text-black font-semibold text-5xl text-start mt-2'>Mohammed!</h1>
+                   <h1 className='text-black text-lg mt-5  font-normal '>From one Host to another – welcome aboard. Thank you for sharing your home and helping to create incredible experiences for our guests.</h1>
+                   </div>
+                    </div>
+                </div>
+
+
+
+
 
             </div>
 
@@ -25,3 +38,11 @@ function Publish(){
 }
 
 export default Publish;
+
+{/* <div className='justify-center flex flex-col items-center'>
+<h1 className='text-white font-semibold text-5xl text-start'>Congratulations,</h1>
+<div className='justify-start flex flex-col '>
+<h1 className='text-white font-semibold text-5xl text-start'>Mohammed!</h1>
+</div>
+
+</div> */}
